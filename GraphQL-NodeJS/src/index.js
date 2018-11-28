638b6323cac9c761f18060bf03c04e789c786ff1
hrg921 <hrg921@gmail.com>
2018-11-28 23:11:50 +0900

    add simple query, mutation

--- GraphQL-NodeJS/src/index.js
+++ GraphQL-NodeJS/src/index.js
@@ -3,23 +3,39 @@
  */
 const { GraphQLServer } = require('graphql-yoga');
 
-// 1
-const typeDefs = `
-type Query {
-  info: String!
-}
-`;
+let links = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL'
+}];
+let idCount = links.length;
 
-// 2
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone`
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+  },
+  Mutation: {
+    post: (root, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      };
+      links.push(link);
+
+      return link
+    }
+  },
+  Link: {
+    id: (root) => root.id,
+    description: (root) => root.description,
+    url: (root) => root.url
   }
 };
 
-// 3
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers
 });
 
